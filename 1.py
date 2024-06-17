4946ace2f2a318c0831930908584ef2d50af30d4
Singh1-Divya <divyasingh876708@gmail.com>
2024-06-17 16:21:21 +0530

    Functions Created

---
+++ 1.py
@@ -0,0 +1,10 @@
+Name = "Divya"
+Age = 19
+Std = "13th"
+Contact = 8767086543
+
+def show():
+    print("Name is:",Name)
+    print("Age is:",Age)
+    print("Std is:",Std)
+    print("Contact is:",Contact)
---
+++ 2.py
@@ -0,0 +1,6 @@
+def even_odd(n):
+    if n%2==0:
+        print("Number is Even.")
+    else:
+        print("Number is Odd.")
+        
